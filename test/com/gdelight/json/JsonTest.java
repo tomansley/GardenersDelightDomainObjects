fdecd6180a164c59b5ae6e8e7fe44fcb99ae3e64
Tom Ansley <tomansley@gmail.com>
2013-02-28 11:10:33 -0600

    Adding JSon test

---
+++ test/com/gdelight/json/JsonTest.java
@@ -0,0 +1,46 @@
+package com.gdelight.json;
+
+import java.util.Date;
+
+import com.gdelight.domain.base.BaseRequestBean.STATUS_TYPE;
+import com.gdelight.domain.base.BaseRequestBean.TRANSACTION_TYPE;
+import com.gdelight.domain.base.StarterRequestBean;
+import com.gdelight.tools.json.JsonUtils;
+
+public class JsonTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JsonTest.createJsonTest();
+	}
+	
+	public static void createJsonTest() {
+		
+		StarterRequestBean bean = new StarterRequestBean();
+		
+		bean.setInternalId("internal ID");
+		bean.setExternalId("External ID");
+		bean.setLastRequestTime(new Date());
+		bean.setRequestTime(new Date());
+		bean.setStatus(STATUS_TYPE.INITIAL);
+		bean.setTransactionType(TRANSACTION_TYPE.LOGIN);
+		
+		String json = JsonUtils.getJSonDocument(bean);
+		
+		System.out.println(json);
+		
+		StarterRequestBean bean2 = (StarterRequestBean) JsonUtils.parseJSonDocument(json, StarterRequestBean.class);
+		
+		System.out.println("Internal ID       = " + bean2.getInternalId());
+		System.out.println("External ID       = " + bean2.getExternalId());
+		System.out.println("Last Request Time = " + bean2.getLastRequestTime());
+		System.out.println("Request Time      = " + bean2.getRequestTime());
+		System.out.println("Status            = " + bean2.getStatus());
+		System.out.println("Transaction Type  = " + bean2.getTransactionType());
+		
+	}
+
+}
